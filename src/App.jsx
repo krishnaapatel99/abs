f8425d14e084d9e6ef45b1805846e93bdf8ce673
krishnaapatel99 <patelkrishu1608@gmail.com>
2026-07-30 08:00:08 +0530

    file changes done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import BackgroundScene from './components/BackgroundScene'
 import Experience from './components/Three/Model/Experience'
 import LandingPage from './components/LandingPage'
-import HeroSection from './components/Hero/HeroSection'
+import HeroSection from './components/Main/HeroSection'
 import './App.css'
 
 function App() {
